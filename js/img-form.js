59123d9796b6f5cc59638408195fd8e1e7e07c05
Vladimir Ilyin <ilyinvovan@gmail.com>
2023-04-10 02:11:47 +0300

    need to save info in form if upload failed

--- js/img-form.js
+++ js/img-form.js
@@ -1,15 +1,14 @@
 // Upload image form validation, handlers etc module
 import '../pristine/pristine.min.js';
 import {isDescriptionValid} from './util.js';
-import {showImageOverlay, hideImageOverlay, showSuccessMessage} from './img-overlay.js';
+import {showImageOverlay, hideImageOverlay, showSuccessMessage, showErrorMessage, blockSubmitButton, unblockSubmitButton} from './img-overlay.js';
 import {form} from './main.js';
+import { postData } from './api.js';
 
 export const imageFormValidate = function () {
   form.querySelector('#upload-file').addEventListener('change', () => {
     showImageOverlay();
   });
-  form.method = 'POST';
-  form.action = 'https://27.javascript.pages.academy/kekstagram-simple';
   const pristine = new Pristine(form, {
     classTo: 'img-upload__text',
     successClass: 'form--valid',
@@ -19,12 +18,20 @@ export const imageFormValidate = function () {
     errorTextTag: 'span'
   });
 
-  pristine.addValidator(form.querySelector('.text__description'), isDescriptionValid, 'Длина должна быть 20-140 символов');
+  pristine.addValidator(
+    form.querySelector('.text__description'),
+    isDescriptionValid,
+    'Длина должна быть 20-140 символов');
+
   form.addEventListener('submit', (evt) => {
     evt.preventDefault();
     if (pristine.validate()) {
+      blockSubmitButton();
+
+      postData(new FormData(evt.target), showSuccessMessage, showErrorMessage);
+
+      unblockSubmitButton();
       hideImageOverlay();
-      showSuccessMessage();
     }
   });
 };
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // Entry point
-import {createPicturesArray} from './data.js';
+
 import {drawPictures} from './draw.js';
-import {imageFormValidate} from './img-form-validate.js';
+import {imageFormValidate} from './img-form.js';
+import {getData} from './api.js';
 
 export const form = document.querySelector('#upload-select-image');
 
-drawPictures(createPicturesArray(25));
+// drawPictures(createPicturesArray(25));
 imageFormValidate();
+getData(drawPictures);
